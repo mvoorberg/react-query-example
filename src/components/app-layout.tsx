4af62db39703ce2b05c249ffe46cc6b9ee3cb3c7
Mark Voorberg <8144503+mvoorberg@users.noreply.github.com>
2025-03-09 20:22:05 -0700

    fix btns, logo

--- src/components/app-layout.tsx
+++ src/components/app-layout.tsx
@@ -6,27 +6,29 @@ export function AppLayout() {
       <header>
         <nav className="container flex justify-between p-4 mx-auto lg:w-screen-lg">
           <Link className="w-full" to="/">
-            <img src="/logo.svg" alt="React Query" width="200" />
+            <img src="/src/logo.png" alt="React Query" width="200" />
           </Link>
           <div className="flex space-x-4">
-            <NavLink
-              className="bg-teal-800 px-4 py-2 text-gray-100 font-semibold rounded-md"
-              to="/"
-            >
-              Basic
-            </NavLink>
-            <NavLink
-              className="bg-teal-800 px-4 py-2 text-gray-100 font-semibold rounded-md"
-              to="/paginated"
-            >
-              Paginated
-            </NavLink>
-            <NavLink
-              className="bg-teal-800 px-4 py-2 text-gray-100 font-semibold rounded-md"
-              to="/infinite"
-            >
-              Infinite
-            </NavLink>
+            <div>
+              <NavLink
+                className="bg-teal-800 mx-1 px-4 py-2 text-gray-100 font-semibold rounded-md"
+                to="/"
+              >
+                Basic
+              </NavLink>
+              <NavLink
+                className="bg-teal-800 mx-1 px-4 py-2 text-gray-100 font-semibold rounded-md"
+                to="/paginated"
+              >
+                Paginated
+              </NavLink>
+              <NavLink
+                className="bg-teal-800 mx-1 px-4 py-2 text-gray-100 font-semibold rounded-md"
+                to="/infinite"
+              >
+                Infinite
+              </NavLink>
+            </div>
           </div>
         </nav>
       </header>
